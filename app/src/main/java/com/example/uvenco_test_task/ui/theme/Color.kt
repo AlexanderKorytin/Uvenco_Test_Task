4c7f15f42bcec4d59ada1788d45f97f514941935
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-25 23:53:43 +0300

    поменял местами цвета

--- app/src/main/java/com/example/uvenco_test_task/ui/theme/Color.kt
+++ app/src/main/java/com/example/uvenco_test_task/ui/theme/Color.kt
@@ -1,6 +1,5 @@
 package com.example.uvenco_test_task.ui.theme
 
-import android.health.connect.datatypes.units.Volume
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -11,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val AppBackground = Color(0xFFb261A18)
+val AppBackground = Color(0xFF261A18)
 val NameTextColor = Color(0xFFC4A79B)
 val Orange = Color(0xFFF77416)
 val VolumeCupColor = Color(0xFF514541)
@@ -21,8 +20,8 @@ val SaveButtonTextColor = Color(0xFFFFFAF7)
 val CheckMarkColor = Color(0xFFFFFFFF)
 val IconCupGradientStart = Color(0xFF19110E)
 val IconCupGradientEnd = Color(0xFF100909)
-val PriceGradientStart = Color(0xFF1D1412)
-val PriceGradientCentre = Color(0xFF160E0C)
+val PriceGradientCentre = Color(0xFF1D1412)
+val PriceGradientStart = Color(0xFF160E0C)
 val PriceGradientEnd = Color(0xFF231917)
 val HeadingColor = Color(0xFF896C61)
 val LabelColor = Color(0xFF5A4036)
