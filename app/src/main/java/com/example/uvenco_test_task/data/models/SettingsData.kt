fc1ce1b8429caa9d7772d9a16b56289c267acb43
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-26 00:01:37 +0300

    добавил поле бесплатности и изменил типы данных

--- app/src/main/java/com/example/uvenco_test_task/data/models/SettingsData.kt
+++ app/src/main/java/com/example/uvenco_test_task/data/models/SettingsData.kt
@@ -6,15 +6,17 @@ import com.example.uvenco_test_task.domain.models.Settings
 data class SettingsData(
     val iconId: IconId = IconId.CAPPUCCINO,
     val drinkName: String = DEFAULT_NAME,
-    val price: String = DEFAULT_PRICE
+    val price: String = DEFAULT_PRICE,
+    val isFree: Boolean = false
 )
 
 fun map(settingsData: SettingsData): Settings {
     return Settings(
         iconId = settingsData.iconId,
         drinkName = settingsData.drinkName,
-        price = settingsData.price,
-        checkMarkList = getCheckMarkList(settingsData.iconId)
+        price = settingsData.price.toDouble(),
+        checkMarkList = getCheckMarkList(settingsData.iconId),
+        isFree = settingsData.isFree && settingsData.price.toDouble() <= ZERO_PRICE
     )
 }
 
@@ -22,7 +24,7 @@ fun map(settings: Settings): SettingsData {
     return SettingsData(
         iconId = settings.iconId,
         drinkName = settings.drinkName,
-        price = settings.price
+        price = String.format("%1.f", settings.price)
     )
 }
 
@@ -31,4 +33,5 @@ private fun getCheckMarkList(iconId: IconId): List<Boolean> {
 }
 
 private const val DEFAULT_NAME = "Капучино"
-private const val DEFAULT_PRICE = "0"
+private const val DEFAULT_PRICE = "199"
+private const val ZERO_PRICE = 0f
--- app/src/main/java/com/example/uvenco_test_task/domain/models/Settings.kt
+++ app/src/main/java/com/example/uvenco_test_task/domain/models/Settings.kt
@@ -3,6 +3,7 @@ package com.example.uvenco_test_task.domain.models
 data class Settings(
     val iconId: IconId,
     val drinkName: String,
-    val price: String,
-    val checkMarkList: List<Boolean>
+    val price: Double,
+    val checkMarkList: List<Boolean>,
+    val isFree: Boolean
 )
