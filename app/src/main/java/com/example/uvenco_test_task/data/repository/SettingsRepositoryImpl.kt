547d3f767e388c5343f7495e0a456ec8380d51e7
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-26 00:00:26 +0300

    убрал не нужные функции

--- app/src/main/java/com/example/uvenco_test_task/data/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/com/example/uvenco_test_task/data/repository/SettingsRepositoryImpl.kt
@@ -1,21 +1,12 @@
 package com.example.uvenco_test_task.data.repository
 
+import com.example.uvenco_test_task.data.models.SettingsData
 import com.example.uvenco_test_task.data.models.map
 import com.example.uvenco_test_task.data.storage.api.SettingsStorage
 import com.example.uvenco_test_task.domain.api.SettingsRepository
 import com.example.uvenco_test_task.domain.models.Settings
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 class SettingsRepositoryImpl(private val storage: SettingsStorage) : SettingsRepository {
-    override suspend fun getTime(): Flow<String> = flow {
-        emit(storage.getTime())
-    }
-
-    override suspend fun getTemperature(): Flow<String> = flow {
-        emit(storage.getTemperature())
-    }
-
     override fun getSettings(): Settings {
         return map(storage.getSettings())
     }
@@ -24,4 +15,7 @@ class SettingsRepositoryImpl(private val storage: SettingsStorage) : SettingsRep
         storage.setString(map(settings))
     }
 
+    override fun getDefaultSettings(): Settings {
+        return map(SettingsData())
+    }
 }
--- app/src/main/java/com/example/uvenco_test_task/domain/impl/SettingsInteractorImpl.kt
+++ app/src/main/java/com/example/uvenco_test_task/domain/impl/SettingsInteractorImpl.kt
@@ -3,23 +3,18 @@ package com.example.uvenco_test_task.domain.impl
 import com.example.uvenco_test_task.domain.api.SettingsInteractor
 import com.example.uvenco_test_task.domain.api.SettingsRepository
 import com.example.uvenco_test_task.domain.models.Settings
-import kotlinx.coroutines.flow.Flow
 
 class SettingsInteractorImpl(private val settingsRepository: SettingsRepository) :
     SettingsInteractor {
-    override suspend fun getTime(): Flow<String> {
-        return settingsRepository.getTime()
-    }
-
-    override suspend fun getTemperature(): Flow<String> {
-        return settingsRepository.getTemperature()
-    }
-
     override fun getSettings(): Settings {
         return settingsRepository.getSettings()
     }
 
     override fun setString(settings: Settings) {
         settingsRepository.setString(settings)
+    }
+
+    override fun getDefaultSettings(): Settings {
+        return settingsRepository.getDefaultSettings()
     }
 }
