fef44de23ed5aa8685e375aa705d0cee2bda0e06
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-25 23:57:31 +0300

    убрал строку состояний

--- app/src/main/java/com/example/uvenco_test_task/MainActivity.kt
+++ app/src/main/java/com/example/uvenco_test_task/MainActivity.kt
@@ -1,52 +1,20 @@
 package com.example.uvenco_test_task
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
-import com.example.uvenco_test_task.presentation.models.MainIntent
-import com.example.uvenco_test_task.presentation.models.MainScreenState
-import com.example.uvenco_test_task.presentation.viewmodels.MainViewModel
-import com.example.uvenco_test_task.ui.theme.AppBackground
-import com.example.uvenco_test_task.ui.theme.NameTextColor
-import org.koin.androidx.compose.getViewModel
-import org.koin.androidx.compose.koinViewModel
+import com.example.uvenco_test_task.ui.screens.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         super.onCreate(savedInstanceState)
         setContent {
             MainScreen()
         }
-    }
-}
-
-@Composable
-internal fun MainScreen(mainViewModel: MainViewModel = koinViewModel()) {
-    mainViewModel.updateScreen(MainIntent.RequestData)
-    when (val state = mainViewModel.mainScreenState.value) {
-        is MainScreenState.Content -> {
-            Box(
-                modifier = Modifier
-                    .background(color = AppBackground)
-                    .fillMaxSize()
-            ) {
-                Text(
-                    text = state.temperature,
-                    fontSize = 64.sp,
-                    modifier = Modifier.align(Alignment.Center),
-                    color = NameTextColor
-                )
-            }
-        }
-
-        MainScreenState.Start -> {}
     }
 }
