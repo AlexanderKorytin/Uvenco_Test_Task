43897eccaf900c9868ba618c1b8d911037711837
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-25 23:55:55 +0300

    написал интерактор и его имплементацию для данных заголовка

---
+++ app/src/main/java/com/example/uvenco_test_task/domain/api/HeadInteractor.kt
@@ -0,0 +1,9 @@
+package com.example.uvenco_test_task.domain.api
+
+import kotlinx.coroutines.flow.Flow
+
+interface HeadInteractor {
+    suspend fun getTime(): Flow<String>
+
+    suspend fun getTemperature(): Flow<String>
+}
---
+++ app/src/main/java/com/example/uvenco_test_task/domain/impl/HeadInteractorImpl.kt
@@ -0,0 +1,15 @@
+package com.example.uvenco_test_task.domain.impl
+
+import com.example.uvenco_test_task.domain.api.HeadInteractor
+import com.example.uvenco_test_task.domain.api.HeadRepository
+import kotlinx.coroutines.flow.Flow
+
+class HeadInteractorImpl(private val headRepository: HeadRepository): HeadInteractor {
+    override suspend fun getTime(): Flow<String> {
+        return headRepository.getTime()
+    }
+
+    override suspend fun getTemperature(): Flow<String> {
+        return headRepository.getTemperature()
+    }
+}
