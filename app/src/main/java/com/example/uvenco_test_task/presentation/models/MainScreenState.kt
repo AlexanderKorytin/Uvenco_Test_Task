441a6b4ea0924ece23f0668977e9a66821c08d87
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-26 00:02:51 +0300

    изменил класс состояния экрана main и убрал код касающийся времени и температуры

--- app/src/main/java/com/example/uvenco_test_task/presentation/models/MainScreenState.kt
+++ app/src/main/java/com/example/uvenco_test_task/presentation/models/MainScreenState.kt
@@ -5,8 +5,6 @@ import com.example.uvenco_test_task.domain.models.Drink
 sealed interface MainScreenState {
     data object Start: MainScreenState
     data class Content(
-        val listDrinks: List<Drink>,
-        val time: String,
-        val temperature: String
+        val listDrinks: List<Drink>
         ): MainScreenState
 }
--- app/src/main/java/com/example/uvenco_test_task/presentation/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/uvenco_test_task/presentation/viewmodels/MainViewModel.kt
@@ -3,15 +3,10 @@ package com.example.uvenco_test_task.presentation.viewmodels
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.uvenco_test_task.domain.api.MainInteractor
 import com.example.uvenco_test_task.presentation.models.MainIntent
 import com.example.uvenco_test_task.presentation.models.MainScreenState
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainViewModel(private val mainInteractor: MainInteractor) : ViewModel() {
     private var _mainScreenState: MutableState<MainScreenState> =
@@ -22,33 +17,16 @@ class MainViewModel(private val mainInteractor: MainInteractor) : ViewModel() {
     fun updateScreen(mainIntent: MainIntent) {
         when (mainIntent) {
             is MainIntent.RequestData -> {
-                if (jobGetDataScreen == null) {
-                    getDataScreen()
-                }
+                getDataScreen()
             }
         }
     }
 
     private fun getDataScreen() {
         val list = mainInteractor.getListDrinks()
-        jobGetDataScreen = viewModelScope.launch(Dispatchers.IO) {
-            while (true) {
-                mainInteractor.getTime().collect { time ->
-                    val temp = mainInteractor.getTemperature().collect { temp ->
-                        withContext(Dispatchers.Main) {
-                            _mainScreenState.value =
-                                MainScreenState.Content(
-                                    temperature = temp,
-                                    time = time,
-                                    listDrinks = list
-                                )
-                        }
-                    }
-                }
-                delay(REQUEST_INTERVAL)
-            }
-        }
+        _mainScreenState.value = MainScreenState.Content(listDrinks = list)
     }
+
 }
 
 private const val REQUEST_INTERVAL = 1000L
