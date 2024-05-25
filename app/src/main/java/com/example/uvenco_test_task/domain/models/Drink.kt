ab310514e612825816c62028d13d779d180ab723
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-25 23:54:44 +0300

    добавил поле

--- app/src/main/java/com/example/uvenco_test_task/domain/models/Drink.kt
+++ app/src/main/java/com/example/uvenco_test_task/domain/models/Drink.kt
@@ -3,5 +3,6 @@ package com.example.uvenco_test_task.domain.models
 data class Drink(
     val iconId: IconId,
     val drinkName: String,
-    val drinkPrice: String
+    val drinkPrice: Double,
+    val isFree: Boolean
 )
