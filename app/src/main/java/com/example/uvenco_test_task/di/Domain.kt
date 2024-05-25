dd5aa531514f6ec88d22a2d179586000e7cefcaf
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-25 23:58:45 +0300

    добавил зависимости

--- app/src/main/java/com/example/uvenco_test_task/di/Domain.kt
+++ app/src/main/java/com/example/uvenco_test_task/di/Domain.kt
@@ -1,11 +1,15 @@
 package com.example.uvenco_test_task.di
 
+import com.example.uvenco_test_task.data.repository.HeadRepositoryImpl
 import com.example.uvenco_test_task.data.repository.MainRepositoryImpl
 import com.example.uvenco_test_task.data.repository.SettingsRepositoryImpl
+import com.example.uvenco_test_task.domain.api.HeadInteractor
+import com.example.uvenco_test_task.domain.api.HeadRepository
 import com.example.uvenco_test_task.domain.api.MainInteractor
 import com.example.uvenco_test_task.domain.api.MainRepository
 import com.example.uvenco_test_task.domain.api.SettingsInteractor
 import com.example.uvenco_test_task.domain.api.SettingsRepository
+import com.example.uvenco_test_task.domain.impl.HeadInteractorImpl
 import com.example.uvenco_test_task.domain.impl.MainInteractorImpl
 import com.example.uvenco_test_task.domain.impl.SettingsInteractorImpl
 import org.koin.dsl.module
@@ -19,11 +23,19 @@ val domainModule = module {
         SettingsRepositoryImpl(storage = get())
     }
 
+    single<HeadRepository> {
+        HeadRepositoryImpl()
+    }
+
     factory<MainInteractor> {
         MainInteractorImpl(mainRepository = get())
     }
 
     factory<SettingsInteractor> {
         SettingsInteractorImpl(settingsRepository = get())
+    }
+
+    factory<HeadInteractor> {
+        HeadInteractorImpl(headRepository = get())
     }
 }
--- app/src/main/java/com/example/uvenco_test_task/di/ViewModelModule.kt
+++ app/src/main/java/com/example/uvenco_test_task/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.example.uvenco_test_task.di
 
+import com.example.uvenco_test_task.presentation.viewmodels.HeadViewModel
 import com.example.uvenco_test_task.presentation.viewmodels.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -7,5 +8,9 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel<MainViewModel> {
         MainViewModel(mainInteractor = get())
+    }
+
+    viewModel<HeadViewModel> {
+        HeadViewModel(headInteractor = get())
     }
 }
