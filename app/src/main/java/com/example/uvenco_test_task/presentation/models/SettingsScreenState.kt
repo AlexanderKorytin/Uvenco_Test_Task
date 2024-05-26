49a16815823ca02fab75398f98ca278b4881e32d
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-26 23:09:04 +0300

    заменил поле в модели

--- app/src/main/java/com/example/uvenco_test_task/presentation/models/SettingsScreenState.kt
+++ app/src/main/java/com/example/uvenco_test_task/presentation/models/SettingsScreenState.kt
@@ -1,15 +1,9 @@
 package com.example.uvenco_test_task.presentation.models
 
-import com.example.uvenco_test_task.domain.models.IconId
+import com.example.uvenco_test_task.domain.models.Settings
 
 sealed interface SettingsScreenState {
     data object Start: SettingsScreenState
 
-    data class Content(
-        val drinkName: String,
-        val drinkPrise: String,
-        val drinkIsFree: Boolean,
-        val checkMarkList: List<Boolean>,
-        val isSettingsChanged: Boolean
-    ): SettingsScreenState
+    data class Content(val settings: Settings): SettingsScreenState
 }
