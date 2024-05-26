9fbef7f01891f6d821507349e40ccd9ce13334c1
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-26 23:09:22 +0300

    добавил интентов

--- app/src/main/java/com/example/uvenco_test_task/presentation/models/SettingsIntent.kt
+++ app/src/main/java/com/example/uvenco_test_task/presentation/models/SettingsIntent.kt
@@ -1,5 +1,9 @@
 package com.example.uvenco_test_task.presentation.models
 
+import com.example.uvenco_test_task.domain.models.Settings
+
 sealed interface SettingsIntent {
     data object RequestData: SettingsIntent
+    data class SetNewData(val newData: Settings): SettingsIntent
+    data object SaveData: SettingsIntent
 }
