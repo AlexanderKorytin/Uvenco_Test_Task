300445fdf519d2a926cbdebd6d17febfde2a6c86
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-26 23:17:37 +0300

    добавил LaunchedEffect

--- app/src/main/java/com/example/uvenco_test_task/ui/screens/Header.kt
+++ app/src/main/java/com/example/uvenco_test_task/ui/screens/Header.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -33,7 +34,9 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ScreenHeader(headViewModel: HeadViewModel = koinViewModel(), onClick: () -> Unit) {
-    headViewModel.updateScreen(HeadIntent.RequestData)
+    LaunchedEffect(key1 = Any()) {
+        headViewModel.updateScreen(HeadIntent.RequestData)
+    }
     when (val state = headViewModel.headScreenState.value) {
         is HeadScreenState.Content -> {
             Row(
